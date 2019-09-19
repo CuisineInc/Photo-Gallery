edaacccd82af8667f27010a1f7d4b3d3b649185d
Soro Didiouf <ssoro013@gmail.com>
2019-09-19 11:47:26 -0700

    Merge conflicts fix

--- server/index.js
+++ server/index.js
@@ -16,12 +16,12 @@ app.use(compression());
 app.use(cors())
 
 app.use(express.static('public'));
-<<<<<<< HEAD
+
 
 app.use('/:listing', express.static('public'));
-=======
+
 // app.use('/:listing', express.static('public'));
->>>>>>> master
+
 
 app.get('/api/restaurants/:id/images', controller.getImages);
 
@@ -35,10 +35,5 @@ app.delete('/api/restaurants/:id/images', controller.deleteImages);
 
 app.delete('/api/images/:id', controller.deleteImage);
 
-<<<<<<< HEAD
 app.listen(port, () => console.log(`App is listening on port ${port}!`));
-=======
 
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
->>>>>>> master
